e488dcc3c3dd268d6430e47856a8afa58c80a596
mfujimoto <GAF04571@nifty.com>
2026-09-13 21:00:47 +0000

    feat: draw histograms on a dedicated overlay, not i-canvas
    Independent Draw histogram checkbox. Overlay uses i-canvas pixel
    contract then scales like render.show. Internal ImageData stays clean.

--- jscam/cam2.js
+++ jscam/cam2.js
@@ -651,12 +651,12 @@ const render = {
  , [255, 255, 255]
   ]
 , histogram: function (frame, histogram) {
- const ic = render.ic
- , gc = ic.getContext('2d')
+ const hc = render.hc
+ , gc = hc.getContext('2d')
  , num = frame.getNum()
  , max = histogram.reduce((a,b)=>(Math.max(a,b)), 0)
- , barScale = (ic.height / 3) / max
- , lineScale = (ic.height / 3) / num
+ , barScale = (hc.height / 3) / max
+ , lineScale = (hc.height / 3) / num
  ;
 
  const rgba = ()=>{
@@ -669,10 +669,10 @@ const render = {
  gc.fillStyle = rgba();
  for (let x = 10, i = 0; i < histogram.length; ++i, ++x) {
   const h = histogram[i] * barScale;
-  gc.fillRect(x, ic.height - 1 - h, 1, h);
+  gc.fillRect(x, hc.height - 1 - h, 1, h);
  }
  let y = histogram[0];
- const yStart = ic.height - 1;
+ const yStart = hc.height - 1;
  gc.strokeStyle = rgba();
  gc.beginPath();
  gc.moveTo(10, yStart - y * lineScale);
@@ -681,6 +681,10 @@ const render = {
   gc.lineTo(x, yStart - y * lineScale);
  }
  gc.stroke();
+}
+, clearHistogram: function () {
+ const hc = render.hc;
+ hc.getContext('2d').clearRect(0, 0, hc.width, hc.height);
 }
 , frame: function (frame) {
  const ic = render.ic
@@ -712,11 +716,16 @@ const render = {
    render.ic.width  = internal[0];
    render.ic.height = internal[1];
   }
+  if (render.hc.width != internal[0] || render.hc.height != internal[1]) {
+   render.hc.width  = internal[0];
+   render.hc.height = internal[1];
+  }
  }
   }
 }
 render.ic = e('i-canvas'); // for internal use
 render.dc = e('d-canvas'); // for display
+render.hc = e('h-canvas'); // histogram overlay (ic bitmap, contain-scaled)
 
 const fitDisplaySize = (videoW, videoH)=>{
  const stage = e('layers');
@@ -786,11 +795,16 @@ dispatch.iDISP = (function * () {
 
   if (dispatch.showImage) {
    render.frame(newFrame);
-   render.histogram.color = 0;
-   for (let k in newFrame.histogram) {
-    render.histogram(newFrame, newFrame.histogram[k]);
-   }
    render.show();
+   if (dispatch.showHistogram) {
+    render.clearHistogram();
+    render.histogram.color = 0;
+    for (let k in newFrame.histogram) {
+     render.histogram(newFrame, newFrame.histogram[k]);
+    }
+   } else {
+    render.clearHistogram();
+   }
   }
 
   //delta.accum(newFrame);
@@ -801,6 +815,7 @@ dispatch.run = false;
 dispatch.paused = false;
 dispatch.count = {'value':0};
 dispatch.showImage = true;
+dispatch.showHistogram = true;
 
 
 const watch = ()=>{
@@ -885,6 +900,11 @@ print.messages = ['','','','','','',''];
 
 e('show-image').onchange = function () {
  dispatch.showImage = this.checked;
+}
+
+e('show-histogram').onchange = function () {
+ dispatch.showHistogram = this.checked;
+ if (!this.checked) render.clearHistogram();
 }
 
 for (let k in buildImageFuncs) {
