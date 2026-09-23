00c4d622dcc25170d7d954b4272893bba0ff4547
mfujimoto <GAF04571@nifty.com>
2026-09-23 10:58:02 +0900

    fix review Issue-7

--- jscam/cam2.js
+++ jscam/cam2.js
@@ -1,8 +1,7 @@
 'use strict';
 
 // Bump on every change (UTC YYYY.MM.DD-HHMMSSZ-<git short HEAD at edit>).
-const JSCAM_VERSION = '2026.09.21-055913Z-1cd9e9f';
-
+const JSCAM_VERSION = '2026.09.23-c1a5ce5bd0f4bc05e1a65d3c8c597e36fef254a8';
 const e = function(tag) {return document.getElementById(tag);}
 
 const Graph = function * (data, label, canvasName, captionName, color) {
